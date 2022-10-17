ad718ef71e4499279fee629d628f49d4e4a4d84b
578Venky <padalavenkatesh578@gmail.com>
2022-10-17 19:02:33 +0530

    Exam

--- assessment4/Angular/multilayout/src/app/admin/employee/employee-routing.module.ts
+++ assessment4/Angular/multilayout/src/app/admin/employee/employee-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { EmployeeComponent } from './employee.component';
 
-
-const routes: Routes = [{path:' ',component:LoginComponent}]
-
-
+const routes: Routes = [{path: "", component:EmployeeComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthenticationRoutingModule { }
+export class EmployeeRoutingModule { }
